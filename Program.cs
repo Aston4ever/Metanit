7f38d538e2c44bc67216edc248df3d3172f927df
aston4ever <aston4ever@gmail.com>
2024-01-26 12:10:20 +0300

    complete: 2.2 vars and const

--- Program.cs
+++ Program.cs
@@ -1,5 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 {
     Console.WriteLine("***************" );
     Console.WriteLine( "Hello, World!" );
 }
+
+int i = 27; 
+float b = 26f;
+double _D = 34.554;     //первый символ должен быть буква или _
+string str = "Aston";
+char c;
+c = 'B';
+Console.WriteLine( i );
+Console.WriteLine( b );
+Console.WriteLine( _D );
+Console.WriteLine( str );
+Console.WriteLine( c );
+c = 'g';
+Console.WriteLine( c );
+const int con = 4; //Нельзя изменять константы
+// con = 5;
+// Должна быть проинициализирована
